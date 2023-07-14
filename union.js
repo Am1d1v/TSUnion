279e27cd96e6844feb3129bbb0cdc33d209dbaa8
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-14 15:52:13 +0300

    Add files via upload
    Union

--- union.js
+++ union.js
@@ -1,6 +1,33 @@
 "use strict";
+// Union
 function logId(id) {
-    console.log(id);
+    if (typeof id === 'string') {
+        console.log('Some String');
+    }
+    else if (typeof id === 'number') {
+        console.log(10);
+    }
+    else {
+        console.log(false);
+    }
 }
 logId(10);
-logId('Name');
+//logId('Name');
+//logId(false);
+function logError(err) {
+    if (Array.isArray(err)) {
+        console.log(err);
+    }
+    else {
+        console.log(err);
+    }
+}
+function logObject(obj) {
+    if ('a' in obj) {
+        console.log(`Object has 'a' key`);
+    }
+}
+function logMultIds(a, b) {
+    if (a === b) {
+    }
+}
--- union.ts
+++ union.ts
@@ -1,8 +1,39 @@
 
 
-function logId(id: string | number){
-    console.log(id);
+
+// Union
+
+function logId(id: string | number | boolean){
+    if(typeof id === 'string'){
+        console.log('Some String');
+    } else if(typeof id === 'number'){
+        console.log(10)
+    } else {
+        console.log(false);
+    }
 }
 
 logId(10);
-logId('Name');
+//logId('Name');
+//logId(false);
+
+function logError(err: string | string[]){
+    if(Array.isArray(err)){
+        console.log(err);
+    } else {
+        console.log(err);
+    }
+}
+
+function logObject(obj: {a: number} | {b: number}){
+    if('a' in obj){
+        console.log(`Object has 'a' key`);
+    }
+}
+
+
+function logMultIds(a: string | number, b: string | number){
+    if (a === b) {
+
+    }
+}
